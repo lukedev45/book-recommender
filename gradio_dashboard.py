48718c1a8088fcb2728a0e2dcaf513775cae3c27
Luke Devlin <lukedevlin45@gmail.com>
2025-08-23 19:44:45 +0100

    Final Version

--- gradio_dashboard.py
+++ gradio_dashboard.py
@@ -3,9 +3,10 @@ import numpy as np
 from dotenv import load_dotenv
 
 from langchain_community.document_loaders import TextLoader
-from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_text_splitters import CharacterTextSplitter
 from langchain_chroma import Chroma
+from langchain_huggingface import HuggingFaceEmbeddings
+embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
 
 import gradio as gr
 
@@ -15,12 +16,12 @@ books = pd.read_csv("books_with_emotions.csv")
 books["large_thumbnail"] = books["thumbnail"] + "&fife=w800"
 books["large_thumbnail"] = np.where(
     books["large_thumbnail"].isna(),
-    "cover_not_found.jpg",
+    "cover-not-found.jpg",
     books["large_thumbnail"],
 )
 
 raw_documents = TextLoader("tagged_description.txt", encoding='utf-8').load()
-text_splitter = CharacterTextSplitter(separator="\n", chunk_size=0, chunk_overlap=0)
+text_splitter = CharacterTextSplitter(separator="\n", chunk_size=1, chunk_overlap=0)
 documents = text_splitter.split_documents(raw_documents)
 db_books = Chroma.from_documents(documents, HuggingFaceEmbeddings())
 
@@ -29,16 +30,16 @@ def retrieve_semantic_recommendations(
         query: str,
         category: str = None,
         tone: str = None,
-        initial_top_k = 50,
-        final_top_k = 16,
+        initial_top_k: int = 50,
+        final_top_k: int = 16,
 ) -> pd.DataFrame:
 
-    recs = db_books.similarity_search_with_score(query, k=initial_top_k)
+    recs = db_books.similarity_search(query, k=initial_top_k)
     books_list = [int(rec.page_content.strip('"').split()[0]) for rec in recs]
-    book_recs = books[books["isbn13"].isin(books_list).head(final_top_k)]
+    book_recs = books[books["isbn13"].isin(books_list)].head(initial_top_k)
 
     if category != "All":
-        book_recs = book_recs[book_recs["simple_categories"] == category][:final_top_k]
+        book_recs = book_recs[book_recs["simple_categories"] == category].head(final_top_k)
     else:
         book_recs = book_recs.head(final_top_k)
 
@@ -55,10 +56,11 @@ def retrieve_semantic_recommendations(
 
     return book_recs
 
+
 def recommend_books(
         query: str,
         category: str,
-        tone: str,
+        tone: str
 ):
     recommendations = retrieve_semantic_recommendations(query, category, tone)
     results = []
@@ -81,24 +83,25 @@ def recommend_books(
     return results
 
 categories = ["All"] + sorted(books["simple_categories"].unique())
-tones = ["All"] +["Happy", "Surprising", "Angry", "Suspenseful", "Sad"]
+tones = ["All"] + ["Happy", "Surprising", "Angry", "Suspenseful", "Sad"]
 
 with gr.Blocks(theme = gr.themes.Glass()) as dashboard:
-    gr.Markdown("# Sematic Book Recommender")
+    gr.Markdown("# Semantic book recommender")
 
     with gr.Row():
-        user_query = gr. Textbox(label = "Please enter a description of a book:",
-                                 placeholder = "e.g., A story about forgiveness")
-        category_dropdown = gr.Dropdown(choices = categories, label = "Category:", value = "All")
-        tone_dropdown = gr.Dropdown(choices = tones, label="Select an emotional tone:", value="All")
-        submit_button = gr.Button("Find Recommendations")
+        user_query = gr.Textbox(label = "Please enter a description of a book:",
+                                placeholder = "e.g., A story about forgiveness")
+        category_dropdown = gr.Dropdown(choices = categories, label = "Select a category:", value = "All")
+        tone_dropdown = gr.Dropdown(choices = tones, label = "Select an emotional tone:", value = "All")
+        submit_button = gr.Button("Find recommendations")
 
-    gr.Markdown('## Recommendations')
-    output = gr.Gallery(label = "Recommended books", columns = 0, rows = 2)
+    gr.Markdown("## Recommendations")
+    output = gr.Gallery(label = "Recommended books", columns = 8, rows = 2)
 
     submit_button.click(fn = recommend_books,
                         inputs = [user_query, category_dropdown, tone_dropdown],
                         outputs = output)
+
 
 if __name__ == "__main__":
     dashboard.launch()
